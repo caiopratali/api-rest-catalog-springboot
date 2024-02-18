b54fc98aecddefea4efe1f7dddf773e13137c046
Caio Pratali <caio@pratali.com.br>
2024-02-18 16:22:46 -0300

    Create CategoryResource

--- src/main/java/br/com/pratali/crud/resources/CategoryResource.java
+++ src/main/java/br/com/pratali/crud/resources/CategoryResource.java
@@ -1,8 +1,24 @@
 package br.com.pratali.crud.resources;
 
+import br.com.pratali.crud.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class CategoryResource {
+import java.util.ArrayList;
+import java.util.List;
 
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = new ArrayList<>();
+
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Electronics"));
+
+        return ResponseEntity.ok().body(list);
+    }
 }
