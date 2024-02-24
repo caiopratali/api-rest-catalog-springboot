3ed724ba1525b9e010e83e5df55bf66fb8dea11b
Caio Pratali <caio@pratali.com.br>
2024-02-24 00:24:28 -0300

    create findCategoryById and exceptionHandler

--- src/main/java/br/com/pratali/crud/services/CategoryService.java
+++ src/main/java/br/com/pratali/crud/services/CategoryService.java
@@ -3,11 +3,13 @@ package br.com.pratali.crud.services;
 import br.com.pratali.crud.dto.CategoryDTO;
 import br.com.pratali.crud.entities.Category;
 import br.com.pratali.crud.repositories.CategoryRepository;
+import br.com.pratali.crud.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,10 +18,16 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
     @Transactional(readOnly = true)
-    public List<CategoryDTO> fingAll () {
+    public List<CategoryDTO> findAll () {
         List<Category> list = repository.findAll();
 
         return list.stream().map(x -> new CategoryDTO(x)).toList();
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
 }
